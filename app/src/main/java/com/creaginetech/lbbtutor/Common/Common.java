197b2fbd6ec452fb0be39c263dec0898b7824b4c
creagine <creaginetech@gmail.com>
2019-05-03 18:19:55 +0700

    03/05 demo

---
+++ app/src/main/java/com/creaginetech/lbbtutor/Common/Common.java
@@ -0,0 +1,11 @@
+package com.creaginetech.lbbtutor.Common;
+
+public class Common {
+
+    public static String currentUser = "";
+    public static String jadwalSelected = "";
+    public static String pekanSelected = "";
+    public static String bulanSelected = "";
+    public static String siswaSelected = "";
+
+}
